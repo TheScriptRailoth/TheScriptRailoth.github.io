e0ae7cbb87e3e5ff7992fa4db25b73d43c6b7b8a
Ashutosh <am3718440@gmail.com>
2024-04-03 15:20:22 +0530

    Portfolio: Added theme switching toggle button
    Signed-off-by: Ashutosh <am3718440@gmail.com>

--- script.js
+++ script.js
@@ -1,34 +1,12 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     const themeToggleButton = document.getElementById('theme-toggle');
-//     const currentTheme = localStorage.getItem('theme') || 'light';
-
-//     if (currentTheme === 'dark') {
-//         document.documentElement.setAttribute('data-theme', 'dark');
-//     }
-
-//     themeToggleButton.addEventListener('click', () => {
-//         const theme = document.documentElement.getAttribute('data-theme') === 'dark' ? 'light' : 'dark';
-//         document.documentElement.setAttribute('data-theme', theme);
-//         localStorage.setItem('theme', theme);
-//     });
-// });
-
-
 document.addEventListener('DOMContentLoaded', () => {
     const toggleCheckbox = document.getElementById('toggle_checkbox');
-    const currentTheme = localStorage.getItem('theme');
+    const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light';
 
-    // Set the initial theme and checkbox state
     if (currentTheme === 'dark') {
         document.documentElement.setAttribute('data-theme', 'dark');
         toggleCheckbox.checked = true;
-    } else {
-        // Ensure light theme is set by default or when explicitly chosen
-        document.documentElement.setAttribute('data-theme', 'light');
-        toggleCheckbox.checked = false;
     }
 
-    // Listen for changes on the checkbox to switch themes
     toggleCheckbox.addEventListener('change', () => {
         if (toggleCheckbox.checked) {
             document.documentElement.setAttribute('data-theme', 'dark');
