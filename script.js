63201d7206ead4858a55ae945c2918b855c23a2c
Ashutosh <am3718440@gmail.com>
2024-04-03 15:32:04 +0530

    Portfolio: updated toggle button functionality
    Signed-off-by: Ashutosh <am3718440@gmail.com>

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
     const toggleCheckbox = document.getElementById('toggle_checkbox');
-    const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light';
 
-    if (currentTheme === 'dark') {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        toggleCheckbox.checked = true;
-    }
+    // Initially set the theme to dark
+    document.documentElement.setAttribute('data-theme', 'dark');
+    toggleCheckbox.checked = true;
 
     toggleCheckbox.addEventListener('change', () => {
         if (toggleCheckbox.checked) {
             document.documentElement.setAttribute('data-theme', 'dark');
-            localStorage.setItem('theme', 'dark');
         } else {
             document.documentElement.setAttribute('data-theme', 'light');
-            localStorage.setItem('theme', 'light');
         }
     });
 });
